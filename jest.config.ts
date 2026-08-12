3fa6598466e42777a597015d173af73c79e10f9c
brandon-alexis <brandonmachado159@gmail.com>
2026-08-12 18:43:10 -0500

    edit: prefijo para reconocimiento de archivos .js relativos por jest

--- jest.config.ts
+++ jest.config.ts
@@ -18,6 +18,17 @@ export default {
     ],
   },
 
+  moduleNameMapper: {
+    '^@src/(.*)\\.js$': '<rootDir>/src/$1',
+    '^@shared/(.*)\\.js$': '<rootDir>/src/shared/$1',
+    '^@config/(.*)\\.js$': '<rootDir>/src/shared/config/$1',
+    '^@students/(.*)\\.js$': '<rootDir>/src/app/students/$1',
+    '^@professors/(.*)\\.js$': '<rootDir>/src/app/professors/$1',
+    '^@items/(.*)\\.js$': '<rootDir>/src/app/items/$1',
+    '^@itemCategories/(.*)\\.js$': '<rootDir>/src/app/itemCategories/$1',
+    '^@itemTypes/(.*)\\.js$': '<rootDir>/src/app/itemTypes/$1',
+  },
+
   testPathIgnorePatterns: ['/dist/', '/node_modules/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
 };
