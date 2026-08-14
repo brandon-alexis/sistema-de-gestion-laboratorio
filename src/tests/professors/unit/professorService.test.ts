d63e56c9f22a1b1b531f925601c7a1adf4851b52
brandon-alexis <brandonmachado159@gmail.com>
2026-08-13 19:36:48 -0500

    add: agregando test unitarios para servicio de profesores y organizacion general de tests

---
+++ src/tests/professors/unit/professorService.test.ts
@@ -0,0 +1,193 @@
+import { Professor } from '@professors/models/Professor.js';
+import type { ProfessorRepository } from '@professors/repositories/ProfessorRepository.js';
+import { UpdateProfessorDto } from '@professors/dtos/UpdateProfessorDto.js';
+import { ProfessorService } from '@professors/services/ProfessorService.js';
+import { ProfessorAlreadyExistsException } from '@professors/exceptions/ProfessorAlreadyExistsException.js';
+import { ProfessorNotFoundException } from '@src/app/professors/exceptions/ProfessorNotFoundException.js';
+
+function createProfessorRepositoryMock(): jest.Mocked<ProfessorRepository> {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findById: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+}
+
+describe('ProfessorService', () => {
+  describe('getAllProfessors', () => {
+    it('deberia devolver todos los profesores', async () => {
+      const professors: Professor[] = [];
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findAll.mockResolvedValue(professors);
+
+      const professorService = new ProfessorService(professorRepository);
+
+      const result = await professorService.getAllProfessors();
+
+      expect(result).toEqual(professors);
+      expect(professorRepository.findAll).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('createProfessor', () => {
+    it('deberia crear un profesor', async () => {
+      const professorId = crypto.randomUUID().toString();
+      const newProfessor = new Professor(
+        professorId,
+        'manolo castilla',
+        new Date(),
+        new Date(),
+      );
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findAll.mockResolvedValue([]);
+      professorRepository.create.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      await professorService.createProfessor(newProfessor);
+    });
+
+    it('deberia lanzar error si el profesor a registrar ya existe', async () => {
+      const professorId = crypto.randomUUID().toString();
+      const newProfessor = new Professor(
+        professorId,
+        'pepe muñoz',
+        new Date(),
+        new Date(),
+      );
+      const professors: Professor[] = [newProfessor];
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findAll.mockResolvedValue(professors);
+      professorRepository.create.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      await expect(
+        professorService.createProfessor(newProfessor),
+      ).rejects.toThrow(ProfessorAlreadyExistsException);
+    });
+  });
+
+  describe('getProfessorById', () => {
+    it('deberia obtener un profesor', async () => {
+      const professorId = crypto.randomUUID().toString();
+      const newProfessor = new Professor(
+        professorId,
+        'manolo guzman',
+        new Date(),
+        new Date(),
+      );
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findById.mockResolvedValue(newProfessor);
+
+      const professorService = new ProfessorService(professorRepository);
+
+      const result = await professorService.getProfessorById(professorId);
+
+      expect(result).toEqual(newProfessor);
+      expect(professorRepository.findById).toHaveBeenCalledTimes(1);
+      expect(professorRepository.findById).toHaveBeenCalledWith(professorId);
+    });
+  });
+
+  describe('updateProfessor', () => {
+    it('deberia actualizar un profesor', async () => {
+      const professorId = crypto.randomUUID().toString();
+      const newProfessor = new Professor(
+        professorId,
+        'manolo guzman',
+        new Date(),
+        new Date(),
+      );
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findAll.mockResolvedValue([]);
+      professorRepository.findById.mockResolvedValue(newProfessor);
+      professorRepository.update.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      const updateProfessorDto = new UpdateProfessorDto('fernando cantillo');
+
+      await professorService.updateProfessor(professorId, updateProfessorDto);
+
+      expect(professorRepository.findAll).toHaveBeenCalledTimes(1);
+      expect(professorRepository.findById).toHaveBeenCalledTimes(1);
+      expect(professorRepository.findById).toHaveBeenCalledWith(professorId);
+      expect(professorRepository.update).toHaveBeenCalledTimes(1);
+      expect(professorRepository.update).toHaveBeenCalledWith(
+        professorId,
+        newProfessor,
+      );
+    });
+
+    it('deberia lanzar error al actualizar un profesor que no existe', async () => {
+      const professorId = crypto.randomUUID().toString();
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findById.mockResolvedValue(null);
+      professorRepository.update.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      const updateProfessorDto = new UpdateProfessorDto('pepe');
+
+      await expect(
+        professorService.updateProfessor(professorId, updateProfessorDto),
+      ).rejects.toThrow(ProfessorNotFoundException);
+    });
+  });
+
+  describe('deleteProfessor', () => {
+    it('deberia eliminar un profesor', async () => {
+      const professorId = crypto.randomUUID().toString();
+      const newProfessor = new Professor(
+        professorId,
+        'pepe gonzalez',
+        new Date(),
+        new Date(),
+      );
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findById.mockResolvedValue(newProfessor);
+      professorRepository.delete.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      await professorService.deleteProfessor(professorId);
+
+      expect(professorRepository.findById).toHaveBeenCalledTimes(1);
+      expect(professorRepository.findById).toHaveBeenCalledWith(professorId);
+      expect(professorRepository.delete).toHaveBeenCalledTimes(1);
+      expect(professorRepository.delete).toHaveBeenCalledWith(professorId);
+    });
+
+    it('deberia lanzar error al eliminar un profesor que no existe', async () => {
+      const professorId = crypto.randomUUID().toString();
+
+      const professorRepository = createProfessorRepositoryMock();
+
+      professorRepository.findById.mockResolvedValue(null);
+      professorRepository.delete.mockResolvedValue();
+
+      const professorService = new ProfessorService(professorRepository);
+
+      await expect(
+        professorService.getProfessorById(professorId),
+      ).rejects.toThrow(ProfessorNotFoundException);
+    });
+  });
+});
--- src/tests/students/unit/studentServive.test.ts
+++ src/tests/students/unit/studentServive.test.ts
@@ -16,189 +16,231 @@ function createdStudentRepositoryMock(): jest.Mocked<StudentRepository> {
 }
 
 describe('StudentService', () => {
-  it('deberia devolver todos los estudiantes', async () => {
-    const students: Student[] = [];
+  describe('getAllStudents', () => {
+    it('deberia devolver todos los estudiantes', async () => {
+      const students: Student[] = [];
 
-    const studentRepository = createdStudentRepositoryMock();
+      const studentRepository = createdStudentRepositoryMock();
 
-    studentRepository.findAll.mockResolvedValue(students);
+      studentRepository.findAll.mockResolvedValue(students);
 
-    const studentService = new StudentService(studentRepository);
+      const studentService = new StudentService(studentRepository);
 
-    const result = await studentService.getAllStudents();
+      const result = await studentService.getAllStudents();
 
-    expect(result).toEqual(students);
-    expect(studentRepository.findAll).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(students);
+      expect(studentRepository.findAll).toHaveBeenCalledTimes(1);
+    });
+
+    it('deberia lanzar error si el estudiante no existe', async () => {
+      const studentId = crypto.randomUUID().toString();
+
+      const studentRepository = createdStudentRepositoryMock();
+
+      studentRepository.findById.mockResolvedValue(null);
+
+      const studentService = new StudentService(studentRepository);
+
+      await expect(studentService.getStudentById(studentId)).rejects.toThrow(
+        StudentNotFoundException,
+      );
+
+      expect(studentRepository.findById).toHaveBeenCalledTimes(1);
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+    });
   });
 
-  it('deberia crear un estudiante', async () => {
-    const studentId = crypto.randomUUID().toString();
-    const studentCreatedAt = new Date();
-    const studentUpdatedAt = new Date();
+  describe('createStudent', () => {
+    it('deberia crear un estudiante', async () => {
+      const studentId = crypto.randomUUID().toString();
+      const studentCreatedAt = new Date();
+      const studentUpdatedAt = new Date();
 
-    const newStudent = new Student(
-      studentId,
-      'pepe gonzales',
-      '123456789',
-      studentCreatedAt,
-      studentUpdatedAt,
-    );
+      const newStudent = new Student(
+        studentId,
+        'pepe gonzales',
+        '123456789',
+        studentCreatedAt,
+        studentUpdatedAt,
+      );
 
-    const studentRepository = createdStudentRepositoryMock();
+      const studentRepository = createdStudentRepositoryMock();
 
-    studentRepository.findAll.mockResolvedValue([]);
-    studentRepository.create.mockResolvedValue();
+      studentRepository.findAll.mockResolvedValue([]);
+      studentRepository.create.mockResolvedValue();
 
-    const studentService = new StudentService(studentRepository);
+      const studentService = new StudentService(studentRepository);
 
-    await studentService.createStudent(newStudent);
+      await studentService.createStudent(newStudent);
 
-    expect(studentRepository.create).toHaveBeenCalledTimes(1);
-    expect(studentRepository.create).toHaveBeenCalledWith(
-      expect.objectContaining({
-        id: studentId,
-        fullname: 'pepe gonzales',
-        createdAt: studentCreatedAt,
-        updatedAt: studentUpdatedAt,
-      }),
-    );
+      expect(studentRepository.create).toHaveBeenCalledTimes(1);
+      expect(studentRepository.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          id: studentId,
+          fullname: 'pepe gonzales',
+          createdAt: studentCreatedAt,
+          updatedAt: studentUpdatedAt,
+        }),
+      );
+    });
+
+    it('deberia lanzar error si el estudiante a registrar ya existe', async () => {
+      const studentId = crypto.randomUUID().toString();
+      const newStudent = new Student(
+        studentId,
+        'pepe',
+        '123456789',
+        new Date(),
+        new Date(),
+      );
+
+      const students: Student[] = [newStudent];
+
+      const studentRepository = createdStudentRepositoryMock();
+
+      studentRepository.findAll.mockResolvedValue(students);
+      studentRepository.create.mockResolvedValue();
+
+      const studentService = new StudentService(studentRepository);
+
+      const createdStudent = new Student(
+        crypto.randomUUID().toString(),
+        'maria',
+        '123456789',
+        new Date(),
+        new Date(),
+      );
+
+      await expect(
+        studentService.createStudent(createdStudent),
+      ).rejects.toThrow(StudentAlreadyExistsException);
+
+      expect(studentRepository.create).not.toHaveBeenCalled();
+    });
   });
 
-  it('deberia obtener un estudiante', async () => {
-    const studentId = crypto.randomUUID().toString();
-    const newStudent = new Student(
-      studentId,
-      'pepe gonzales',
-      '123456789',
-      new Date(),
-      new Date(),
-    );
-    const students: Student[] = [newStudent];
+  describe('getStudentById', () => {
+    it('deberia obtener un estudiante', async () => {
+      const studentId = crypto.randomUUID().toString();
+      const newStudent = new Student(
+        studentId,
+        'pepe gonzales',
+        '123456789',
+        new Date(),
+        new Date(),
+      );
+      const students: Student[] = [newStudent];
 
-    const studentRepository = createdStudentRepositoryMock();
+      const studentRepository = createdStudentRepositoryMock();
 
-    studentRepository.findAll.mockResolvedValue(students);
-    studentRepository.findById.mockResolvedValue(newStudent);
+      studentRepository.findAll.mockResolvedValue(students);
+      studentRepository.findById.mockResolvedValue(newStudent);
 
-    const studentService = new StudentService(studentRepository);
+      const studentService = new StudentService(studentRepository);
 
-    const result = await studentService.getStudentById(studentId);
+      const result = await studentService.getStudentById(studentId);
 
-    expect(result).toEqual(newStudent);
-    expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
-    expect(studentRepository.findById).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(newStudent);
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+      expect(studentRepository.findById).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('deberia actualizar el estudiante', async () => {
-    const studentId = crypto.randomUUID().toString();
-    const studentCreatedAt = new Date();
-    const newStudent = new Student(
-      studentId,
-      'maria sanches',
-      '12345678999',
-      studentCreatedAt,
-      new Date(),
-    );
-    const students: Student[] = [newStudent];
+  describe('updateStudent', () => {
+    it('deberia actualizar el estudiante', async () => {
+      const studentId = crypto.randomUUID().toString();
+      const newStudent = new Student(
+        studentId,
+        'maria sanches',
+        '12345678999',
+        new Date(),
+        new Date(),
+      );
+      const students: Student[] = [newStudent];
 
-    const studentRepository = createdStudentRepositoryMock();
+      const studentRepository = createdStudentRepositoryMock();
 
-    studentRepository.findAll.mockResolvedValue(students);
-    studentRepository.findById.mockResolvedValue(newStudent);
-    studentRepository.update.mockResolvedValue();
+      studentRepository.findAll.mockResolvedValue(students);
+      studentRepository.findById.mockResolvedValue(newStudent);
+      studentRepository.update.mockResolvedValue();
 
-    const studentService = new StudentService(studentRepository);
+      const studentService = new StudentService(studentRepository);
 
-    const updateStudentDto = new UpdateStudentDto('luis felipe', '12345');
+      const updateStudentDto = new UpdateStudentDto('luis felipe', '12345');
 
-    await studentService.updateStudent(studentId, updateStudentDto);
+      await studentService.updateStudent(studentId, updateStudentDto);
 
-    expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
-    expect(studentRepository.findById).toHaveBeenCalledTimes(1);
+      expect(studentRepository.findAll).toHaveBeenCalledTimes(1);
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+      expect(studentRepository.findById).toHaveBeenCalledTimes(1);
 
-    expect(studentRepository.update).toHaveBeenCalledWith(
-      studentId,
-      newStudent,
-    );
-    expect(studentRepository.update).toHaveBeenCalledTimes(1);
+      expect(studentRepository.update).toHaveBeenCalledWith(
+        studentId,
+        newStudent,
+      );
+      expect(studentRepository.update).toHaveBeenCalledTimes(1);
+    });
+
+    it('deberia lanzar error al actualizar un estudiante que no existe', async () => {
+      const studentId = crypto.randomUUID();
+      const studentRepository = createdStudentRepositoryMock();
+
+      studentRepository.findById.mockResolvedValue(null);
+
+      const studentService = new StudentService(studentRepository);
+
+      const updateStudentDto = new UpdateStudentDto('luis felipe', '12345');
+
+      await expect(
+        studentService.updateStudent(studentId, updateStudentDto),
+      ).rejects.toThrow(StudentNotFoundException);
+
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+      expect(studentRepository.update).not.toHaveBeenCalled();
+    });
   });
 
-  it('deberia eliminar un estudiante', async () => {
-    const studentId = crypto.randomUUID().toString();
-    const newStudent = new Student(
-      studentId,
-      'laura sanchez',
-      '1010101010',
-      new Date(),
-      new Date(),
-    );
-    const students: Student[] = [newStudent];
+  describe('deleteStudent', () => {
+    it('deberia eliminar un estudiante', async () => {
+      const studentId = crypto.randomUUID().toString();
+      const newStudent = new Student(
+        studentId,
+        'laura sanchez',
+        '1010101010',
+        new Date(),
+        new Date(),
+      );
 
-    const studentRepository = createdStudentRepositoryMock();
+      const studentRepository = createdStudentRepositoryMock();
 
-    studentRepository.findById.mockResolvedValue(newStudent);
-    studentRepository.findAll.mockResolvedValue(students);
-    studentRepository.delete.mockResolvedValue();
+      studentRepository.findById.mockResolvedValue(newStudent);
+      studentRepository.delete.mockResolvedValue();
 
-    const studentService = new StudentService(studentRepository);
+      const studentService = new StudentService(studentRepository);
 
-    await studentService.deleteStudent(studentId);
+      await studentService.deleteStudent(studentId);
 
-    expect(studentRepository.findById).toHaveBeenCalledTimes(1);
-    expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+      expect(studentRepository.findById).toHaveBeenCalledTimes(1);
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
 
-    expect(studentRepository.delete).toHaveBeenCalledTimes(1);
-    expect(studentRepository.delete).toHaveBeenCalledWith(studentId);
-  });
+      expect(studentRepository.delete).toHaveBeenCalledTimes(1);
+      expect(studentRepository.delete).toHaveBeenCalledWith(studentId);
+    });
 
-  it('deberia lanzar error si el estudiante no existe', async () => {
-    const studentId = crypto.randomUUID().toString();
+    it('deberia lanzar error al eliminar un estudiante que no existe', async () => {
+      const studentId = crypto.randomUUID();
+      const studentRepository = createdStudentRepositoryMock();
 
-    const studentRepository = createdStudentRepositoryMock();
+      studentRepository.findById.mockResolvedValue(null);
 
-    studentRepository.findById.mockResolvedValue(null);
+      const studentService = new StudentService(studentRepository);
 
-    const studentService = new StudentService(studentRepository);
+      await expect(studentService.deleteStudent(studentId)).rejects.toThrow(
+        StudentNotFoundException,
+      );
 
-    await expect(studentService.getStudentById(studentId)).rejects.toThrow(
-      StudentNotFoundException,
-    );
-
-    expect(studentRepository.findById).toHaveBeenCalledTimes(1);
-    expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
-  });
-
-  it('deberia lanzar error si el estudiante a registrar ya existe', async () => {
-    const studentId = crypto.randomUUID().toString();
-    const newStudent = new Student(
-      studentId,
-      'pepe',
-      '123456789',
-      new Date(),
-      new Date(),
-    );
-
-    const students: Student[] = [newStudent];
-
-    const studentRepository = createdStudentRepositoryMock();
-
-    studentRepository.findAll.mockResolvedValue(students);
-    studentRepository.create.mockResolvedValue();
-
-    const studentService = new StudentService(studentRepository);
-
-    const createdStudent = new Student(
-      crypto.randomUUID().toString(),
-      'maria',
-      '123456789',
-      new Date(),
-      new Date(),
-    );
-
-    await expect(studentService.createStudent(createdStudent)).rejects.toThrow(
-      StudentAlreadyExistsException,
-    );
-
-    expect(studentRepository.create).not.toHaveBeenCalled();
+      expect(studentRepository.findById).toHaveBeenCalledWith(studentId);
+      expect(studentRepository.delete).not.toHaveBeenCalled();
+    });
   });
 });
